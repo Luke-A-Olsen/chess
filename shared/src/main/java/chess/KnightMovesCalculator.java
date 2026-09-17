6b51b7672bb47976651cd4cdf41a8772c5ce568d
Luke Olsen <lukeolsen77@gmail.com>
2026-09-17 17:29:02 -0600

    Added functionality to KnightMoves class

--- shared/src/main/java/chess/KnightMovesCalculator.java
+++ shared/src/main/java/chess/KnightMovesCalculator.java
@@ -7,6 +7,27 @@ public class KnightMovesCalculator
 {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition start)
     {
-        return new ArrayList<>();
+        Collection<ChessMove> moves = new ArrayList<>();
+        ChessPiece me = board.getPiece(start);
+        int[][] offsets = {{2, 1}, {2, -1}, {-2, 1}, {-2, -1}, {1, 2}, {1, -2}, {-1, 2}, {-1, -2}};
+
+        for (int[] offset : offsets)
+        {
+            int row = start.getRow() + offset[0];
+            int col = start.getColumn() + offset[1];
+
+            if (row >= 1 && row <= 8 && col >= 1 && col <= 8)
+            {
+                ChessPosition end = new ChessPosition(row, col);
+                ChessPiece target = board.getPiece(end);
+
+                if (target == null || target.getTeamColor() != me.getTeamColor())
+                {
+                    moves.add(new ChessMove(start, end, null));
+                }
+            }
+        }
+
+        return moves;
     }
 }
