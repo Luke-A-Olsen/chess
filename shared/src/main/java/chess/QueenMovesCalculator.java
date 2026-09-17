7c2bbd6f916ab162ee95b14d996179ca52e94b3e
Luke Olsen <lukeolsen77@gmail.com>
2026-09-17 11:33:10 -0600

    Implemented bishop moves and started on queen moves

--- shared/src/main/java/chess/QueenMovesCalculator.java
+++ shared/src/main/java/chess/QueenMovesCalculator.java
@@ -7,6 +7,38 @@ public class QueenMovesCalculator
 {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition start)
     {
-        return new ArrayList<>();
+        Collection<ChessMove> moves = new ArrayList<>();
+        ChessPiece me = board.getPiece(start);
+        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {-1, -1}, {1, 1}};
+
+        for (int[] dir : directions)
+        {
+            int row = start.getRow() + dir[0];
+            int col = start.getColumn() + dir[1];
+
+            while (row >= 1 && row <= 8 && col >= 1 && col <= 8)
+            {
+                ChessPosition end = new ChessPosition(row, col);
+                ChessPiece target = board.getPiece(end);
+
+                if (target == null)
+                {
+                    moves.add(new ChessMove(start, end, null));
+                }
+                else
+                {
+                    if (target.getTeamColor() != me.getTeamColor())
+                    {
+                        moves.add(new ChessMove(start, end, null));
+                    }
+                    break;
+                }
+
+                row += dir[0];
+                col += dir[1];
+            }
+        }
+
+        return moves;
     }
 }
