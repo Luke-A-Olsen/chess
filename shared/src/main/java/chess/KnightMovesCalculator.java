4d0f83ef77f12a65fcf904a2964e934886526b74
Luke Olsen <lukeolsen77@gmail.com>
2026-09-17 11:09:10 -0600

    Created each piece's calculator class, implemented the full code for RookMoves

---
+++ shared/src/main/java/chess/KnightMovesCalculator.java
@@ -0,0 +1,12 @@
+package chess;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class KnightMovesCalculator
+{
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition start)
+    {
+        return new ArrayList<>();
+    }
+}
---
+++ shared/src/main/java/chess/RookMovesCalculator.java
@@ -0,0 +1,44 @@
+package chess;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class RookMovesCalculator
+{
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition start)
+    {
+        Collection<ChessMove> moves = new ArrayList<>();
+        ChessPiece me = board.getPiece(start);
+        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+        for (int[] dir : directions)
+        {
+            int row = start.getRow() + dir[0];
+            int col = start.getColumn() + dir[1];
+
+            while (row >= 1 && row <= 8 && col >= 1 && col <= 8)
+            {
+                ChessPosition end = new ChessPosition(row, col);
+                ChessPiece target = board.getPiece(end);
+
+                if (target == null)
+                {
+                    moves.add(new ChessMove(start, end, null));
+                }
+                else
+                {
+                    if (target.getTeamColor() != me.getTeamColor())
+                    {
+                        moves.add(new ChessMove(start, end, null));
+                    }
+                    break;
+                }
+
+                row += dir[0];
+                col += dir[1];
+            }
+        }
+
+        return moves;
+    }
+}
