72712bde15f8ec7f721240c6a0fc40f9c6aad31f
Luke Olsen <lukeolsen77@gmail.com>
2026-09-17 18:03:08 -0600

    Added functionality to KingMovesCalculator

--- shared/src/main/java/chess/KingMovesCalculator.java
+++ shared/src/main/java/chess/KingMovesCalculator.java
@@ -7,6 +7,27 @@ public class KingMovesCalculator
 {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition start)
     {
-        return new ArrayList<>();
+        Collection<ChessMove> moves = new ArrayList<>();
+        ChessPiece me = board.getPiece(start);
+        int[][] offsets = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+
+        for (int[] offset : offsets)
+        {
+            int row = start.getRow() + offset[0];
+            int col = start.getColumn() + offset[1];
+
+            if (row >= 1 && row <= 8 && col >= 1 && col <= 8)
+            {
+                ChessPosition end = new ChessPosition(row, col);
+                ChessPiece target = board.getPiece(end);
+
+                if (target == null || target.getTeamColor() != me.getTeamColor())
+                {
+                    moves.add(new ChessMove(start, end, null));
+                }
+            }
+        }
+
+        return moves;
     }
 }
